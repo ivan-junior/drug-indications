71f7879e4ae292f9b636ad32f8795e28ce12d585
Ivan Junior <ivanbjunior.93@gmail.com>
2025-05-18 23:20:12 -0300

    Added @nestjs/throttler package for rate limiting in the application. Configured ThrottlerModule in AppModule with a limit of 100 requests per minute and integrated ThrottlerGuard for global request throttling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,19 @@ import { IndicationsModule } from './indications/indications.module';
 import { ScraperModule } from './scraper/scraper.module';
 import { MappingModule } from './mapping/mapping.module';
 import { ProgramsModule } from './programs/programs.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     AuthModule,
     UsersModule,
     DrugsModule,
@@ -24,6 +32,6 @@ import { ProgramsModule } from './programs/programs.module';
     ProgramsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
